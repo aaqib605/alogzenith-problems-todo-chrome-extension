ee5e16ae09f272bbaf39e498ffe3a8c6785a7b09
aaqibjavaid <aaqibjavaid605@gmail.com>
2025-06-04 11:07:56 +0530

    feat: inject bookmark button in problems page

---
+++ content.js
@@ -0,0 +1,23 @@
+addBookmarkButton();
+
+function addBookmarkButton() {
+  if (!onProblemsPage() || document.getElementById("bookmark-button")) return;
+
+  const problemNav = document.querySelector(
+    ".coding_nav_bg__HRkIn.p-2.nav.nav-pills.w-100 ul"
+  );
+
+  const bookmarkButton = document.createElement("img");
+  bookmarkButton.src = chrome.runtime.getURL("assets/bookmark.png");
+  bookmarkButton.alt = "bookmark-button";
+  bookmarkButton.id = "bookmark-button";
+  bookmarkButton.style.height = "30px";
+  bookmarkButton.style.width = "30px";
+  bookmarkButton.style.cursor = "pointer";
+
+  problemNav.appendChild(bookmarkButton);
+}
+
+function onProblemsPage() {
+  return window.location.pathname.startsWith("/problems");
+}
