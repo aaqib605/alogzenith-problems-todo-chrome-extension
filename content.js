b14684abde7305b69803f16375518d1f1a24610d
aaqibjavaid <aaqibjavaid605@gmail.com>
2025-06-04 11:44:24 +0530

    feat: implement saving problem in chrome storage

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+const AZ_PROBLEMS = "AZ_PROBLEMS";
+
 const observer = new MutationObserver(() => addBookmarkButton());
 
 observer.observe(document.body, { childList: true, subtree: true });
@@ -20,8 +22,49 @@ function addBookmarkButton() {
   bookmarkButton.style.cursor = "pointer";
 
   problemNav.appendChild(bookmarkButton);
+
+  bookmarkButton.addEventListener("click", addNewBookmark);
 }
 
 function onProblemsPage() {
   return window.location.pathname.startsWith("/problems");
+}
+
+async function addNewBookmark() {
+  const existingBookmarks = await getCurrentBookmarks();
+
+  const problemName = document.querySelector(
+    ".coding_problem_info_heading__G9ueL.fw-bolder.rubik.fs-4.mb-0"
+  ).textContent;
+  const problemUrl = window.location.href;
+  const uniqueProblemId = extractUniqueProblemId(problemUrl);
+
+  if (existingBookmarks.find((bookmark) => bookmark.id === uniqueProblemId))
+    return;
+
+  const problemObj = {
+    name: problemName,
+    url: problemUrl,
+    id: uniqueProblemId,
+  };
+
+  const updatedBookmarks = [...existingBookmarks, problemObj];
+
+  chrome.storage.sync.set({ AZ_PROBLEMS: updatedBookmarks }, () => {
+    console.log("Problem added to bookmarks", updatedBookmarks);
+  });
+}
+
+function getCurrentBookmarks() {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get([AZ_PROBLEMS], (result) => {
+      resolve(result[AZ_PROBLEMS] || []);
+    });
+  });
+}
+
+function extractUniqueProblemId(url) {
+  const pathname = new URL(url).pathname;
+  const segments = pathname.split("/");
+  return segments.pop();
 }
